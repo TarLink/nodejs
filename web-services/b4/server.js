8f9a6b0b1ef15c0bd2e3cd4886feb88ebf199fba
Alexandru Bucur <aleks.bucur27@gmail.com>
2018-05-25 20:01:15 +0300

    create web service I accidentally deleted this commit so I cheat and commit the files from book sourcecode :)

--- web-services/b4/server.js
+++ web-services/b4/server.js
@@ -1,20 +1,28 @@
-'use strict'
-const express = require('express')
-const morgan = require('morgan')
-const nconf = require('nconf')
-const pkg = require('./package.json')
+/***
+ * Excerpted from "Node.js 8 the Right Way",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/jwnode2 for more book information.
+***/
+'use strict';
+const express = require('express');
+const morgan = require('morgan');
+const nconf = require('nconf');
+const pkg = require('./package.json');
 
-nconf.argv().env('__')
-nconf.defaults({conf: `${__dirname}/config.json`})
-nconf.file(nconf.get('conf'))
+nconf.argv().env('__');
+nconf.defaults({conf: `${__dirname}/config.json`});
+nconf.file(nconf.get('conf'));
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.get('/api/version', (req, res) => res.status(200).send(pkg.version))
+app.get('/api/version', (req, res) => res.status(200).send(pkg.version));
 
-require('./lib/search.js')(app, nconf.get('es'))
-require('./lib/bundle.js')(app, nconf.get('es'))
+require('./lib/search.js')(app, nconf.get('es'));
+require('./lib/bundle.js')(app, nconf.get('es'));
 
-app.listen(nconf.get('port'), () => console.log('Ready.'))
+app.listen(nconf.get('port'), () => console.log('Ready.'));
